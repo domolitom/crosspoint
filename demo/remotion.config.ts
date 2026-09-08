125ea0c4dba763e7a5838bb8b16bae8bc62e8016
Oliver Domokos <domolitom@gmail.com>
2026-09-08 16:25:14 +0300

    gitignore the demo app

--- demo/remotion.config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from '@remotion/cli/config';
-
-Config.setVideoImageFormat('jpeg');
-Config.setOverwriteOutput(true);
-// Playwright's Chromium is already on this machine; no reason to download a second one.
-Config.setChromiumOpenGlRenderer('angle');
